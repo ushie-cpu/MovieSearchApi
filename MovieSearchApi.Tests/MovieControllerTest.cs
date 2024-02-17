241d621b0fe4a308f427857ad8b26915cb57acff
john.ushie_kuda <john.ushie_kuda@DESKTOP-M0OA61C>
2024-02-17 15:52:50 +0100

    add unit test project

---
+++ MovieSearchApi.Tests/MovieControllerTest.cs
@@ -0,0 +1,62 @@
+﻿using Entity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using Services;
+using Xunit;
+
+namespace MovieSearchApi.Controllers
+{
+    public class MovieControllerTest
+    {
+        [Fact]
+        public async Task SearchMovieById_ReturnsOkObjectResult()
+        {
+            // Arrange
+            var omdbServiceMock = new Mock<OmdbService>(MockBehavior.Strict, new HttpClient());
+            omdbServiceMock.Setup(s => s.SearchMovieByIdAsync(It.IsAny<string>()))
+                           .ReturnsAsync(new Movie { Title = "Sample Movie" });
+
+            var memoryCacheMock = new Mock<IMemoryCache>();
+            var controller = new MovieController(omdbServiceMock.Object, memoryCacheMock.Object);
+
+            // Act
+            var result = await controller.SearchMovie("tt123456", null, null, null);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+            var okObjectResult = (OkObjectResult)result.Result;
+            Assert.IsType<Movie>(okObjectResult.Value);
+            var movie = (Movie)okObjectResult.Value;
+            Assert.Equal("Sample Movie", movie.Title);
+        }
+
+        [Fact]
+        public async Task SearchMovieByTitle_ReturnsOkObjectResultAndSavesSearchQuery()
+        {
+            // Arrange
+            var omdbServiceMock = new Mock<OmdbService>(MockBehavior.Strict, new HttpClient());
+            omdbServiceMock.Setup(s => s.SearchMovieByTitleAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                           .ReturnsAsync(new Movie { Title = "Sample Movie" });
+
+            var memoryCacheMock = new Mock<IMemoryCache>();
+            memoryCacheMock.Setup(c => c.Get<List<SearchQuery>>("LatestSearchQueries"))
+                           .Returns(new List<SearchQuery>());
+            memoryCacheMock.Setup(c => c.Set("LatestSearchQueries", It.IsAny<List<SearchQuery>>(), It.IsAny<MemoryCacheEntryOptions>()));
+
+            var controller = new MovieController(omdbServiceMock.Object, memoryCacheMock.Object);
+
+            // Act
+            var result = await controller.SearchMovie(null, "Sample Movie", null, null);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+            var okObjectResult = (OkObjectResult)result.Result;
+            Assert.IsType<Movie>(okObjectResult.Value);
+            var movie = (Movie)okObjectResult.Value;
+            Assert.Equal("Sample Movie", movie.Title);
+
+            memoryCacheMock.Verify(c => c.Set("LatestSearchQueries", It.IsAny<List<SearchQuery>>(), It.IsAny<MemoryCacheEntryOptions>()), Times.Once);
+        }
+    }
+}
